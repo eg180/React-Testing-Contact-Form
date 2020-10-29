06298342c9981dc9322ad2f3955e1c820be9d7ce
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-29 16:19:53 -0400

    initial test passing for first name

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,17 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import 'mutationobserver-shim';
+import { render, screen, fireEvent } from '@testing-library/react';
+import ContactForm from './components/ContactForm';
+
+test('Animal form accepts text in the form  fields', ()=> {
+    render(<ContactForm />);
+
+
+    const firstNameInput = screen.getByLabelText(/First Name*/i);
+})
+
+
